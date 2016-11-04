5fa29840c29c0baf0f2c2d99318ee3d04b6bad95
Cleiton Domazak <cleiton.domazak@contaazul.com>
2016-11-04 14:20:00 -0200

    Update pgbadger.sh

--- Monitoring/pgbadger/pgbadger.sh
+++ Monitoring/pgbadger/pgbadger.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 BUCKET="s3://<your_bucket_name>/"
 PGBADGERLOGS="<storage_pgbadger_logs>"
-INCREMENTAL=$1
-JOBS=$2
-RETENTION=$3
+JOBS=$1
+RETENTION=$2
 
 execute_analyze () {
-    if [[ ${INCREMENTAL} -eq 1 && ${JOBS} -ne '' && ${RETENTION} -ne '' ]]; then
+    if [[ ${JOBS} -ne '' && ${RETENTION} -ne '' ]]; then
             pgbadger -j ${JOBS} --exclude-query="^(COPY|COMMIT)" -I -O ${PGBADGERLOGS} -R ${RETENTION} ${PGDATA}/pg_log/postgresql-$(date +%a).log
             aws s3 sync ${PGBADGERLOGS} ${BUCKET}
     else
@@ -14,4 +13,4 @@ execute_analyze () {
 fi
 }
 
-execute_analyze ${INCREMENTAL} ${JOBS} ${RETENTION}
+execute_analyze ${JOBS} ${RETENTION}
